8607de50102271eb9c88d53b5f6728fc5f8d00fb
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-10-27 16:25:48 -0400

    outputs a beautiful, ready to count list

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+from os import linesep, sep
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -24,22 +27,86 @@ print("hello world")
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     with open(file) as text:
-        lines = text.readlines()
-        print(f"{len(lines)} lines in the file.")
+        lines = text.read()
+        # print(f"{len(lines)} lines in the file.")
         # print(lines)
-        for line in lines:
-            line = line.replace(",", " ")
-            line = line.replace(".", " ")
-            line = line.replace("_", " ")
-            line = line.replace("?", " ")
-            line = line.replace(":", " ")
-            line = line.replace("’", " ")
-            line = line.replace("-", " ")
-            line = line.lower()
-            line = line.split(' ')
-            # line = line.remove("brim")
-            print(line)
+        # for line in lines:
+        lines = lines.replace(",", " ")
+        lines = lines.replace(".", " ")
+        lines = lines.replace("_", " ")
+        lines = lines.replace("?", " ")
+        lines = lines.replace(":", " ")
+        lines = lines.replace("’", "")
+        lines = lines.replace("-", " ")
+        lines = lines.replace("\n", " ")
+        lines = lines.lower()
 
+        lines = lines.split(' ')
+
+
+# correct use of a for loop would work here with STOP_WORDS
+        while 'a' in lines:
+            lines.remove('a')
+        while 'an' in lines:
+            lines.remove('an')
+        while 'and' in lines:
+            lines.remove('and')
+        while 'are' in lines:
+            lines.remove('are')
+        while 'as' in lines:
+            lines.remove('as')
+        while 'at' in lines:
+            lines.remove('at')
+        while 'be' in lines:
+            lines.remove('be')
+        while 'by' in lines:
+            lines.remove('by')
+        while 'for' in lines:
+            lines.remove('for')
+        while 'from' in lines:
+            lines.remove('from')
+        while 'has' in lines:
+            lines.remove('has')
+        while 'he' in lines:
+            lines.remove('he')
+        while 'i' in lines:
+            lines.remove('i')
+        while 'in' in lines:
+            lines.remove('in')
+        while 'is' in lines:
+            lines.remove('is')
+        while 'it' in lines:
+            lines.remove('it')
+        while 'its' in lines:
+            lines.remove('its')
+        while 'of' in lines:
+            lines.remove('of')
+        while 'on' in lines:
+            lines.remove('on')
+        while 'that' in lines:
+            lines.remove('that')
+        while 'the' in lines:
+            lines.remove('the')
+        while 'to' in lines:
+            lines.remove('to')
+        while 'were' in lines:
+            lines.remove('were')
+        while 'will' in lines:
+            lines.remove('will')
+        while 'with' in lines:
+            lines.remove('with')
+        while '' in lines:
+            lines.remove('')
+
+            
+
+
+
+
+        # while 'a' == False: 
+        #     lines.remove('a')
+
+        print(lines)
 
 
 if __name__ == "__main__":
